6bf4e4608dabab5bb6ea126b0c299f2809656327
sinmu <3038267725@qq.com>
2022-05-19 18:39:28 +0800

    使用JWT

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -9,6 +9,8 @@ use App\Http\Requests\Api\SocialAuthorizationRequest;
 use Illuminate\Support\Arr;
 use Overtrue\LaravelSocialite\Socialite;
 use App\Models\User;
+use App\Http\Requests\Api\AuthorizationRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthorizationsController extends Controller
 {
@@ -57,7 +59,49 @@ class AuthorizationsController extends Controller
 
                 break;
         }
+        // TODO 这里为什么就可以获取token了呢？
+        $token = auth('api')->login($user);
+        return $this->responseWithToken($token)->setStatusCode(201);
 
         return response()->json(['token' => $user->id]);
+    }
+
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = Auth::guard('api')->attempt($credentials)) {
+            throw new AuthenticationException('用户名或密码错误');
+        }
+
+        return $this->responseWithToken($token)->setStatusCode(201);
+
+    }
+
+    public function responseWithToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            // TODO 这里为什么可以直接使用 factory 方法呢？
+            'expires_in' => auth('api')->factory()->getTTL() *60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = auth('api')->refresh();
+        return $this->responseWithToken($token);
+    }
+
+    public function destroy()
+    {
+        auth('api')->logout();
+        return response(null, 204);
     }
 }
